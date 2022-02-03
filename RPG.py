4493b6c7bbeb43bc1ee0891b8bc05ab937f654bf
Arnaud <arnogay@gmail.com>
2022-02-03 15:53:56 +0100

    push files

---
+++ RPG.py
@@ -0,0 +1,27 @@
+class PlayerRPG:
+    def __init__(self):
+        pass
+
+class Monster:
+    def __init__(self):
+        pass
+
+class Use_Potion():
+    def __init__(self):
+        pass
+
+class Moves:
+    def __init__(self):
+        pass
+
+class Chest:
+    def __init__(self):
+        pass
+
+class Donjon:
+    def __init__(self):
+        pass
+
+class Fight:
+    def __init__(self):
+        pass
